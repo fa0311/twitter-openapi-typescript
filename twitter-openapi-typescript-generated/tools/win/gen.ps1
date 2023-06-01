825ed415e487cb1018e559b044e7d82703a18573
ふぁ <yuki@yuki0311.com>
2023-06-02 05:47:01 +0900

    add git
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter-openapi-typescript-generated/tools/win/gen.ps1
+++ twitter-openapi-typescript-generated/tools/win/gen.ps1
@@ -3,6 +3,6 @@
 Start-Process -FilePath "python" -ArgumentList "tools/build.py" -WorkingDirectory "twitter-openapi" -Wait -NoNewWindow
 
 tools/win/clean.ps1
-java -jar tools/openapi-generator-cli.jar generate -g typescript-fetch -c tools/openapi-generator-config.yaml
+java -jar tools/openapi-generator-cli.jar generate -g typescript-fetch -c tools/openapi-generator-config.yaml --git-repo-id twitter-openapi-typescript --git-user-id fa0311
 python tools/win/replace.py
 npm install

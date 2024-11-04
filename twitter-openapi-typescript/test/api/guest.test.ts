8589613dcedc4f1c75917f8679e689ef2c6dd8b1
ふぁ <yuki@yuki0311.com>
2024-11-05 00:36:12 +0900

    add test
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter-openapi-typescript/test/api/guest.test.ts
+++ twitter-openapi-typescript/test/api/guest.test.ts
@@ -1,10 +1,18 @@
 import { getGuestClient, logger } from '@test/init';
-import { printTweet } from '@test/util';
+import { printTweet, printUser } from '@test/util';
 
 test('getGuestTweetResultByRestId', async () => {
   logger.log('getGuestTweetResultByRestId');
   const client = await getGuestClient();
   const response = await client.getDefaultApi().getTweetResultByRestId({ tweetId: '1349129669258448897' });
   printTweet(response.data!);
+  expect(response.raw.response.ok).toBe(true);
+});
+
+test('getGuestUserByScreenName', async () => {
+  logger.log('getGuestUserByScreenName');
+  const client = await getGuestClient();
+  const response = await client.getUserApi().getUserByScreenName({ screenName: 'elonmusk' });
+  printUser(response.data);
   expect(response.raw.response.ok).toBe(true);
 });
